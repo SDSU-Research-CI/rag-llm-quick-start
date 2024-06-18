ab869debe89ab56b2843641e3457d104fc5c2145
David Goldberg <56214889+davmgoldberg@users.noreply.github.com>
2024-06-18 15:38:53 -0700

    Update vector_database_setup.py

--- vector_database_setup.py
+++ vector_database_setup.py
@@ -1,36 +1,35 @@
-import csv, IPython.display, ollama, pip, os
-from tqdm.notebook import tqdm
-import chromadb, nltk
+import chromadb, csv, nltk, ollama, os
+from tqdm import tqdm
 from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
-nltk.download("punkt", quiet = True) # Note: would be nice to load onto the notebook by default in the future to avoid this step
+nltk.download("punkt", quiet = True)
 from nltk.tokenize import sent_tokenize
 
-# This will clear the Chroma database
-myfile = "/chroma/chroma.sqlite3"
-# If file exists, delete it.
-if os.path.isfile(myfile):
-    os.remove(myfile)
+# Remove old file, if applicable
+if os.path.isfile("/chroma/chroma.sqlite3"):
+    os.remove("/chroma/chroma.sqlite3")
 
 # Collect student responses from CSV
-# Test dataset included one qualitative question. TO DO: add the other questions
-documents = []
-with open("student_responses.csv", "r") as file:
-  reader = csv.reader(file)
-  for line in reader:
-    text_response = line[0] # TO DO: add other fields and experiment with filters
-    documents.append(text_response)
+questions, levels, years, colleges, time_bases, campuses, ages, residencies, living_situations, smart_devices, documents = [], [], [], [], [], [], [], [], [], [], []
+with open("student_responses_all_detail.csv", "r") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        for i, var in enumerate((questions, levels, years, colleges, time_bases, campuses, ages, residencies, living_situations, smart_devices, documents)):
+            var.append(line[i])
 
 # Create vector database
-client = chromadb.PersistentClient(path="/chroma",settings=Settings())
+client = chromadb.PersistentClient(path = "/chroma", settings = Settings())
 collection = client.create_collection(name = "docs")
 
-# Vectorize responses by sentence-level chunks and store in vector database
-for i, document in enumerate(documents):
-  for j, sentence in enumerate(sent_tokenize(document)):
-    response = ollama.embeddings(model = "nomic-embed-text", prompt = sentence)
-    embedding = response["embedding"]
-    collection.add(
-      ids = [f"{i}_{j}"],
-      embeddings = [embedding],
-      documents = [document]
-    )
+# Vectorize responses by sentence-level chunks and store in database
+for i, document in enumerate(tqdm(documents)):
+    for j, sentence in enumerate(sent_tokenize(document)):
+        response = ollama.embeddings(model = "nomic-embed-text", prompt = sentence)
+        embedding = response["embedding"]
+        collection.add(
+            ids = [f"{i}_{j}"],
+            embeddings = [embedding],
+            documents = [document],
+            metadatas = [{"question": questions[i], "level": levels[i], "year": years[i], "college": colleges[i], 
+                         "time_basis": time_bases[i], "campus": campuses[i], "age": ages[i], "residency": residencies[i],
+                         "living_situation": living_situations[i], "smart_devices": smart_devices[i]}]
+        )
