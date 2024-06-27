ebcd589aeca0e36c99ec9bd75ea225b04c0b2000
David Goldberg <56214889+davmgoldberg@users.noreply.github.com>
2024-06-27 11:52:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ head = """
 <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
 <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 <link rel = "stylesheet" href = "https://aisurvey.sdsu.edu/css/style_chatbot.css" />
-<base target = "_PARENT" />
 """
 
 # Custom scripts
